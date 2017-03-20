2e4cac923aff9d2cd87fbbb57cf9ce38c88911fa
Yonah Karp <YonahKarp@Yks-MacBook-Air.local>
2017-03-20 09:42:35 -0400

    started working on the map

--- core/src/com/pacman/gameObjects/Map.java
+++ core/src/com/pacman/gameObjects/Map.java
@@ -8,5 +8,15 @@ import java.util.ArrayList;
  * Created by YonahKarp on 3/13/17.
  */
 public class Map {
-
+    //                  123456789abcde
+    String mapString = "╔════════════╦╤════════════╗"+
+                       "║ "+
+                       "║"+
+                       "║"+
+                       "║"+
+                       "║"+
+                       "║"+
+                       "║"+
+                       "║"+
+                       "╙";
 }
