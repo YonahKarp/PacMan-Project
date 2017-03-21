1f6629e52b2cf8e5c27e53748a85073a43b7f190
Yonah Karp <YonahKarp@Yks-MacBook-Air.local>
2017-03-20 21:35:42 -0400

    worked more on ascii map implementation

--- core/src/com/pacman/Services/AssetLoader.java
+++ core/src/com/pacman/Services/AssetLoader.java
@@ -13,25 +13,25 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class AssetLoader {
 
-    public static Texture texture;
+    public static Texture pacmanSheet, mazeSheet;
 
     public static Animation<TextureRegion> pacmAnimation;
     public static Animation<TextureRegion> dyingPacmAnimation;
 
     public static TextureRegion pacOpen, pacMid, pacClosed;
+    public static TextureRegion[][] mazeTiles;
 
     public static void load() {
 
-        texture = new Texture(Gdx.files.internal("pacmanSheet.png"));
-        texture.setFilter(TextureFilter.Nearest, TextureFilter.Nearest); //retain pixelation (no blur)
+        pacmanSheet = new Texture(Gdx.files.internal("pacmanSheet.png"));
+        pacmanSheet.setFilter(TextureFilter.Nearest, TextureFilter.Nearest); //retain pixelation (no blur)
 
-
-        pacOpen = new TextureRegion(texture, 3, 1, 13, 13); //location on sprite sheet
+        pacOpen = new TextureRegion(pacmanSheet, 3, 1, 13, 13); //location on sprite sheet
         //pacOpen.flip(false, true); //may need to be flipped in Y down paradigm
 
-        pacMid = new TextureRegion(texture, 19, 1, 13, 13);;
+        pacMid = new TextureRegion(pacmanSheet, 19, 1, 13, 13);;
 
-        pacClosed = new TextureRegion(texture, 35, 1, 13, 13);
+        pacClosed = new TextureRegion(pacmanSheet, 35, 1, 13, 13);
 
         TextureRegion[] pacMan = { pacOpen, pacMid, pacClosed };
         pacmAnimation = new Animation<TextureRegion>(0.1f, pacMan);
@@ -41,24 +41,30 @@ public class AssetLoader {
         /*
          *  Dead Pacman
          */
-
         TextureRegion[] dyingPacman = {
-                new TextureRegion(texture, 51, 1, 13, 13),
-                new TextureRegion(texture, 67, 1, 13, 13),
-                new TextureRegion(texture, 83, 1, 13, 13),
-                new TextureRegion(texture, 99, 1, 13, 13),
-                new TextureRegion(texture, 115, 1, 13, 13),
-                new TextureRegion(texture, 131, 1, 13, 13),
-                new TextureRegion(texture, 147, 1, 13, 13),
-                new TextureRegion(texture, 163, 1, 13, 13),
-                new TextureRegion(texture, 179, 1, 13, 13),
-                new TextureRegion(texture, 196, 1, 13, 13),
-                new TextureRegion(texture, 212, 1, 13, 13),
+                new TextureRegion(pacmanSheet, 51, 1, 13, 13),
+                new TextureRegion(pacmanSheet, 67, 1, 13, 13),
+                new TextureRegion(pacmanSheet, 83, 1, 13, 13),
+                new TextureRegion(pacmanSheet, 99, 1, 13, 13),
+                new TextureRegion(pacmanSheet, 115, 1, 13, 13),
+                new TextureRegion(pacmanSheet, 131, 1, 13, 13),
+                new TextureRegion(pacmanSheet, 147, 1, 13, 13),
+                new TextureRegion(pacmanSheet, 163, 1, 13, 13),
+                new TextureRegion(pacmanSheet, 179, 1, 13, 13),
+                new TextureRegion(pacmanSheet, 196, 1, 13, 13),
+                new TextureRegion(pacmanSheet, 212, 1, 13, 13),
         };
 
-        dyingPacmAnimation = new Animation<TextureRegion>(0.1f, dyingPacman);
+        dyingPacmAnimation = new Animation<>(0.1f, dyingPacman);
         dyingPacmAnimation.setPlayMode(Animation.PlayMode.NORMAL); //nom nom nom
 
+        /*
+         *  Map
+         */
+        final int FRAME_COLS = 13, FRAME_ROWS = 3;
+        mazeTiles = TextureRegion.split(mazeSheet,
+                mazeSheet.getWidth() / FRAME_COLS,
+                mazeSheet.getHeight() / FRAME_ROWS);
 
 
 
@@ -68,7 +74,7 @@ public class AssetLoader {
 
     public static void dispose() {
         // Dispose of texture when finished. File is 'large'
-        texture.dispose();
+        pacmanSheet.dispose();
     }
 
 }
